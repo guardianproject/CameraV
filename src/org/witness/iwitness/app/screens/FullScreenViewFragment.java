d1c4ffa05a3265f8156c1ad635f394d341c876e9
harlo <LvH@VIDEOCLASH.local>
2013-03-29 05:51:33 -0400

    starting to reincorporate the image editor

--- src/org/witness/iwitness/app/screens/FullScreenViewFragment.java
+++ src/org/witness/iwitness/app/screens/FullScreenViewFragment.java
@@ -1,85 +1,222 @@
 package org.witness.iwitness.app.screens;
 
+import org.witness.informacam.InformaCam;
+import org.witness.informacam.models.IMedia;
 import org.witness.iwitness.R;
+import org.witness.iwitness.app.EditorActivity;
 import org.witness.iwitness.utils.Constants.App;
+import org.witness.iwitness.utils.Constants.App.Editor.Mode;
 import org.witness.iwitness.utils.Constants.Codes;
 import org.witness.iwitness.utils.Constants.EditorActivityListener;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.media.ExifInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-public class FullScreenViewFragment extends Fragment implements OnClickListener {
+public class FullScreenViewFragment extends Fragment implements OnClickListener, OnTouchListener {
 	View rootView;
 	Activity a;
-	
+
+	InformaCam informaCam;
+	IMedia media;
+	Bitmap bitmap, originalBitmap, previewBitmap;
+	int[] dims;
+
 	ImageButton toggleControls;
 	LinearLayout controlsHolder;
+	ImageView mediaHolder;
 	boolean controlsAreShowing = false;
-	
-	@SuppressWarnings("unused")
+
+	// sample sized used to downsize from native photo
+	int inSampleSize;
+
+	// Image Matrix
+	Matrix matrix = new Matrix();
+
+	// Saved Matrix for not allowing a current operation (over max zoom)
+	Matrix savedMatrix = new Matrix();
+
+	// We can be in one of these 3 states
+	int mode = Mode.NONE;
+
+	// For Zooming
+	float startFingerSpacing = 0f;
+	float endFingerSpacing = 0f;
+	PointF startFingerSpacingMidPoint = new PointF();
+
+	// For Dragging
+	PointF startPoint = new PointF();
+
+	// Don't allow it to move until the finger moves more than this amount
+	// Later in the code, the minMoveDistance in real pixels is calculated
+	// to account for different touch screen resolutions
+	float minMoveDistanceDP = 5f;
+	float minMoveDistance; // = ViewConfiguration.get(this).getScaledTouchSlop();
+
+	//handles threaded events for the UI thread
+	private Handler mHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+
+			switch (msg.what) {
+			case 1: // loaded?
+				Log.d(LOG, "bitmap loaded");
+				break;
+			default:
+				super.handleMessage(msg);
+			}
+		}
+
+	};
+
 	private final static String LOG = App.Editor.LOG;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater li, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(li, container, savedInstanceState);
-		
+
 		rootView = li.inflate(R.layout.fragment_editor_fullscreen_view, null);
 		toggleControls = (ImageButton) rootView.findViewById(R.id.toggle_controls);
-		
+
 		int controlHolder = R.id.controls_holder_landscape;
-		
+
 		if(getArguments().getInt(Codes.Extras.SET_ORIENTATION) == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
 			controlHolder = R.id.controls_holder_portrait;
 		}
-		
+
 		controlsHolder = (LinearLayout) rootView.findViewById(controlHolder);
-				
+
+		mediaHolder = (ImageView) rootView.findViewById(R.id.media_holder);
+
 		return rootView;
 	}
-	
+
 	@Override
 	public void onAttach(Activity a) {
 		super.onAttach(a);
 		this.a = a;
-		
+
+		media = ((EditorActivity) a).media;
+		informaCam = InformaCam.getInstance();
 	}
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
-		
+
 		if(getArguments().getInt(Codes.Extras.SET_ORIENTATION) == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
 			controlsHolder = (LinearLayout) rootView.findViewById(R.id.controls_holder_landscape);
 		} else {
 			controlsHolder = (LinearLayout) rootView.findViewById(R.id.controls_holder_portrait);
 		}
-		
+
 		initLayout();
-		
+
 	}
-	
+
 	private void initLayout() {
 		toggleControls.setOnClickListener(this);
 		toggleControls();
+
+		BitmapFactory.Options bfo = new BitmapFactory.Options();
+		bfo.inJustDecodeBounds = true;
+		bfo.inPreferredConfig = Bitmap.Config.RGB_565;
+
+		info.guardianproject.iocipher.FileInputStream bFile = informaCam.ioService.getFileInputStream(media.bitmap); 
+		bitmap = BitmapFactory.decodeStream(bFile);
+
+		dims = informaCam.getDimensions();
+		// Ratios between the display and the image
+		double widthRatio =  Math.floor(bfo.outWidth / dims[0]);
+		double heightRatio = Math.floor(bfo.outHeight / dims[1]);
+
+		// If both of the ratios are greater than 1,
+		// one of the sides of the image is greater than the screen
+		if (heightRatio > widthRatio) {
+			// Height ratio is larger, scale according to it
+			inSampleSize = (int)heightRatio;
+		} else {
+			// Width ratio is larger, scale according to it
+			inSampleSize = (int)widthRatio;
+		}
+
+		bfo.inSampleSize = inSampleSize;
+		bfo.inJustDecodeBounds = false;
+
+		bitmap = BitmapFactory.decodeStream(bFile, null, bfo);
+
+		if (media.dcimEntry.exif.orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+			Log.d(LOG, "Rotating Bitmap 90");
+			Matrix rotateMatrix = new Matrix();
+			rotateMatrix.postRotate(90);
+			bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),rotateMatrix,false);
+		} else if (media.dcimEntry.exif.orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+			Log.d(LOG,"Rotating Bitmap 270");
+			Matrix rotateMatrix = new Matrix();
+			rotateMatrix.postRotate(270);
+			bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),rotateMatrix,false);
+		}
+
+		originalBitmap = bitmap;
+		setBitmap();
 	}
-	
+
+	private void setBitmap() {
+		float matrixWidthRatio = (float) dims[0] / (float) bitmap.getWidth();
+		float matrixHeightRatio = (float) dims[1] / (float) bitmap.getHeight();
+
+		// Setup the imageView and matrix for scaling
+		float matrixScale = matrixHeightRatio;
+
+		if (matrixWidthRatio < matrixHeightRatio) {
+			matrixScale = matrixWidthRatio;
+		} 
+
+		mediaHolder.setImageBitmap(bitmap);
+
+		// Set the OnTouch and OnLongClick listeners to this (ImageEditor)
+		mediaHolder.setOnTouchListener(this);
+		mediaHolder.setOnClickListener(this);
+
+
+		//PointF midpoint = new PointF((float)imageBitmap.getWidth()/2f, (float)imageBitmap.getHeight()/2f);
+		matrix.postScale(matrixScale, matrixScale);
+
+		// This doesn't completely center the image but it get's closer
+		//int fudge = 42;
+		matrix.postTranslate((float)((float) dims[0] -(float) bitmap.getWidth() * (float) matrixScale)/2f,(float)((float) dims[1] - (float) bitmap.getHeight() * matrixScale)/2f);
+
+		mediaHolder.setImageMatrix(matrix);
+	}
+
 	private void toggleControls() {
 		int d = R.drawable.ic_edit_show_tags;
-		
+
 		if(controlsAreShowing) {
 			controlsHolder.setVisibility(View.GONE);
 			d = R.drawable.ic_edit_hide_tags;
@@ -88,7 +225,7 @@ public class FullScreenViewFragment extends Fragment implements OnClickListener 
 			controlsHolder.setVisibility(View.VISIBLE);
 			controlsAreShowing = true;
 		}
-		
+
 		toggleControls.setImageDrawable(a.getResources().getDrawable(d));
 	}
 
@@ -97,7 +234,13 @@ public class FullScreenViewFragment extends Fragment implements OnClickListener 
 		if(v == toggleControls) {
 			toggleControls();
 		}
-		
+
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- src/org/witness/iwitness/utils/Constants.java
+++ src/org/witness/iwitness/utils/Constants.java
@@ -4,16 +4,16 @@ import org.witness.informacam.models.IMedia;
 
 public class Constants {	
 	public interface EditorActivityListener {
-		
+
 	}
-	
+
 	public interface HomeActivityListener {
 		public int[] getDimensions();
 		public void launchEditor(IMedia media);
 		public void logoutUser();
 		public void getContextualMenuFor(String mediaId);
 	}
-	
+
 	public class Codes {
 		public class Routes {
 			public static final int HOME = 1;
@@ -24,11 +24,11 @@ public class Constants {
 			public static final int LOGOUT = org.witness.informacam.utils.Constants.Codes.Messages.Login.DO_LOGOUT;
 			public static final int WIPE = 4;
 		}
-		
+
 		public class Media {
 			public static final int ORIENTATION_PORTRAIT = org.witness.informacam.utils.Constants.Codes.Media.ORIENTATION_PORTRAIT;
 			public static final int ORIENTATION_LANDSCAPE = org.witness.informacam.utils.Constants.Codes.Media.ORIENTATION_LANDSCAPE;
-			
+
 			public static final int TYPE_IMAGE = org.witness.informacam.utils.Constants.Codes.Media.TYPE_IMAGE;
 			public static final int TYPE_VIDEO = org.witness.informacam.utils.Constants.Codes.Media.TYPE_VIDEO;
 			public static final int TYPE_JOURNAL = org.witness.informacam.utils.Constants.Codes.Media.TYPE_JOURNAL;
@@ -42,44 +42,61 @@ public class Constants {
 			public static final String RETURNED_MEDIA = org.witness.informacam.utils.Constants.Codes.Extras.RETURNED_MEDIA;
 		}
 	}
-	
+
 	public class Utils {
 		public final static String LOG = "******************** iWitness : Utils ********************";
 	}
-	
+
 	public class Preferences {
 		public class Keys {
 			public final static String ORIGINAL_IMAGE_HANDLING = "originalImageHandling";
 		}
-		
+
 		public class OriginalImageHandling {
 			public final static int DELETE_ORIGINAL = 0;
 			public final static int SAVE_ORIGINAL = 1;
 		}
 	}
-	
-	
+
+
 	public class App {
 		public class Camera {
 			public final static String LOG = "******************** iWitness : CameraActivity ********************";
 			public static final int ROUTE_CODE = Codes.Routes.CAMERA;
 		}
-		
+
 		public class Editor {
 			public final static String LOG = "******************** iWitness : EditorActivity ********************";
 			public static final int ROUTE_CODE = Codes.Routes.EDITOR;
+
+			public class Mode {
+				public static final int NONE = 0;
+				public static final int DRAG = 1;
+				public static final int ZOOM = 2;
+				public static final int TAP = 3;
+			}
+			
+			public class Color {
+				public final static int DRAW_COLOR = 0x00000000;
+				public final static int DETECTED_COLOR = 0x00000000;
+				public final static int OBSCURED_COLOR = 0x00000000;
+			}
+
+
+			// Maximum zoom scale
+			public static final float MAX_SCALE = 10f;
 		}
-		
+
 		public class Home {
 			public final static String LOG = "******************** iWitness : HomeActivity ********************";
 			public static final int ROUTE_CODE = Codes.Routes.HOME;
 			public static final String TAG = "iWitness.Home";
-			
+
 			public class Tabs {
 				public class UserManagement {
 					public final static String TAG = App.UserManagement.TAG;
 				}
-				
+
 				public class Gallery {
 					public final static String TAG = App.Gallery.TAG;
 				}
@@ -89,29 +106,29 @@ public class Constants {
 				}
 			}
 		}
-		
+
 		public class UserManagement {
 			public final static String TAG = "user_management";
 		}
-		
+
 		public class Gallery {
 			public final static String TAG = "gallery";
 		}
-		
+
 		public class CameraChooser {
 			public final static String TAG = "camera_chooser";
 		}
-		
+
 		public class Router {
 			public final static String LOG = "******************** iWitness : Router ********************";
 		}
-		
-		
+
+
 		public class Wizard {
 			public final static String LOG = "******************** iWitness : WizardActivity ********************";
 			public static final int ROUTE_CODE = Codes.Routes.WIZARD;
 		}
-		
+
 		public class Login {
 			public final static String LOG = "******************** iWitness : LoginActivity ********************";
 			public static final int ROUTE_CODE = Codes.Routes.LOGIN;
