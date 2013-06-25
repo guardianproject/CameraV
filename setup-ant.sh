54b6c6eab919803ab1588a84fd1b1d6a68e54488
n8fr8 <nathan@freitas.net>
2013-06-25 02:29:50 -0400

    modifed setup script and InformaCam peg

--- setup-ant.sh
+++ setup-ant.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 for f in `find external/ -name project.properties`; do
-    android update lib-project -p `dirname $f`
+    android update lib-project -p `dirname $f` -t android-17
 done
-android update project -p app/ --subprojects
+android update project -p app/ -t android-17 --subprojects
+
+cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
+
