8ccc2c7d5de3609088d53b85dada17c9f1f07119
n8fr8 <nathan@freitas.net>
2013-07-02 09:59:01 -0400

    attempts to deal with support jar versioning

--- setup-ant.sh
+++ setup-ant.sh
@@ -6,4 +6,8 @@ done
 android update project -p app/ -t android-17 --subprojects
 
 cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
+cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libonionkit/libs/android-support-v4.jar
+cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/CacheWord/cachewordlib/libs/android-support-v4.jar
 
+rm -rf external/ActionBarSherlock/actionbarsherlock-samples
+rm -rf app/bin/dexedLibs/android-support-v4*
