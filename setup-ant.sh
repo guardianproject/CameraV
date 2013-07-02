d1b4c40010e9f194d65b0f71202e1a8744ba4d32
n8fr8 <nathan@freitas.net>
2013-07-02 14:49:58 -0400

    updated ant script to set proper binary name

--- setup-ant.sh
+++ setup-ant.sh
@@ -3,7 +3,7 @@
 for f in `find external/ -name project.properties`; do
     android update lib-project -p `dirname $f` -t android-17
 done
-android update project -p app/ -t android-17 --subprojects
+android update project -p app/ -t android-17 -n eyeWitness --subprojects
 
 cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libonionkit/libs/android-support-v4.jar
