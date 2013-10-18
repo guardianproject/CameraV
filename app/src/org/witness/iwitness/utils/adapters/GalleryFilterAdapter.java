fadecd9e763b68a16966ee21e63141a59df85bcb
mikaelvonpfaler <mikael.von.pfaler@gmail.com>
2013-10-18 10:20:21 +0200

    Lots of gallery UI fixes
    Task #2061

---
+++ app/src/org/witness/iwitness/utils/adapters/GalleryFilterAdapter.java
@@ -0,0 +1,56 @@
+package org.witness.iwitness.utils.adapters;
+
+import org.witness.iwitness.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class GalleryFilterAdapter extends BaseAdapter {
+	private Context mContext;
+	private CharSequence[] mObjects;
+
+	public GalleryFilterAdapter(Context context, CharSequence[] objects) {
+		super();
+		mContext = context;
+		mObjects = objects;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null)
+			view = LayoutInflater.from(mContext).inflate(
+					R.layout.actionbar_filter_dropdown, parent, false);
+		return view;
+	}
+
+	@Override
+	public View getDropDownView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null)
+			view = LayoutInflater.from(mContext).inflate(
+					R.layout.sherlock_spinner_dropdown_item, parent, false);
+
+		((TextView) view).setText((CharSequence) getItem(position));
+		return view;
+	}
+
+	@Override
+	public int getCount() {
+		return mObjects.length;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mObjects[position];
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return getItem(position).hashCode();
+	}
+}
