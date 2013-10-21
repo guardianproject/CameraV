86e4b55ecafbb25c1cf415f4e911aaf04e216dd2
n8fr8 <nathan@freitas.net>
2013-10-21 12:51:43 -0400

    remove dup iocipher jars

--- setup-ant.sh
+++ setup-ant.sh
@@ -9,5 +9,8 @@ cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/ac
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libonionkit/libs/android-support-v4.jar
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/CacheWord/cachewordlib/libs/android-support-v4.jar
 
+rm external/InformaCam/external/CacheWord/cachewordlib/libs/iocipher.jar
+
 rm -rf external/ActionBarSherlock/actionbarsherlock-samples
 rm -rf app/bin/dexedLibs/android-support-v4*
+
