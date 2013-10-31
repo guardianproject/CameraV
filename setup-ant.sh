7196f57b21b804c52b2fa6cb6a620a7221a37ac0
n8fr8 <nathan@freitas.net>
2013-10-31 01:21:29 -0400

    ensure iocipher jars are the same version

--- setup-ant.sh
+++ setup-ant.sh
@@ -8,6 +8,8 @@ android update project -p app/ -t android-18 -n InformaCam --subprojects
 cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libonionkit/libs/android-support-v4.jar
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/CacheWord/cachewordlib/libs/android-support-v4.jar
+cp external/InformaCam/libs/iocipher.jar external/InformaCam/external/CacheWord/cachewordlib/libs/iocipher.jar
+
 
 rm -rf external/ActionBarSherlock/actionbarsherlock-samples
 rm -rf app/bin/dexedLibs/android-support-v4*
