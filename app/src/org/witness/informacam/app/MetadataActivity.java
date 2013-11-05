b1c5ed78a515df4a8e23878fa4d096ba06a95806
n8fr8 <nathan@freitas.net>
2013-11-05 04:30:49 -0500

    added new metadata simple text view activity

---
+++ app/src/org/witness/informacam/app/MetadataActivity.java
@@ -0,0 +1,47 @@
+package org.witness.informacam.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.ClipboardManager;
+import android.text.method.ScrollingMovementMethod;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MetadataActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_metadata);
+	}
+
+	@Override
+	protected void onResume() {
+		
+		super.onResume();
+		
+		String displayText = "";
+		
+
+		if (getIntent() != null && getIntent().hasExtra("title"))
+		{
+			setTitle(getIntent().getStringExtra("title"));
+		}
+		
+		
+		if (getIntent() != null && getIntent().hasExtra("text"))
+		{
+			displayText = getIntent().getStringExtra("text");
+		}
+		
+		TextView txtView = (TextView)findViewById(R.id.textarea_metadata);
+		txtView.setText(displayText);
+		
+		txtView.setMovementMethod(new ScrollingMovementMethod());
+	}
+
+}
