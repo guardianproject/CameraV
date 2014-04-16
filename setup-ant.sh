2ac14a0f3d347031886a83f6a8dc718284e2de17
n8fr8 <nathan@freitas.net>
2014-04-15 23:42:00 -0400

    updating setup to android-19

--- setup-ant.sh
+++ setup-ant.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 for f in `find external/ -name project.properties`; do
-    android update lib-project -p `dirname $f` -t android-18
+    android update lib-project -p `dirname $f` -t android-19
 done
-android update project -p app/ -t android-18 -n InformaCam --subprojects
+android update project -p app/ -t android-19 -n InformaCam --subprojects
 
 cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libnetcipher/libs/android-support-v4.jar
