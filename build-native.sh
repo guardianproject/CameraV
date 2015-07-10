73385a1f6953d93d4fecf430196c30c7ac5c8368
n8fr8 <nathan@freitas.net>
2015-07-10 15:37:58 -0400

    we are no longer building IOCipher here

--- build-native.sh
+++ build-native.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-make -C external/InformaCam/external/IOCipher/external
-ndk-build -C external/InformaCam/external/IOCipher
+##make -C external/InformaCam/external/IOCipher/external
+##ndk-build -C external/InformaCam/external/IOCipher
 ndk-build -C external/InformaCam
 
 cd external/InformaCam/external/android-ffmpeg-java/external/android-ffmpeg/
